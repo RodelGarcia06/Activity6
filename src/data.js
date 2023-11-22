de16565f0d8b30a2e8e43586ed46c30abbedacb5
RodelGarcia06 <143071000+RodelGarcia06@users.noreply.github.com>
2023-11-22 17:45:21 +0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,89 @@
+const data = [
+    {
+      id: 1,
+      name: {
+        firstname: 'Arnold James',
+        lastname: 'Felisilda',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'AJ123',
+      password: 'arjamesd123',
+      usertype: 'Teacher'
+    },
+    {
+      id: 2,
+      name: {
+        firstname: 'Adrian Jane',
+        lastname: 'Tahil',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'Jane123',
+      password: 'Adie456',
+      usertype: 'Student'
+    },
+    {
+      id: 3,
+      name: {
+        firstname: 'Joana',
+        lastname: 'Jagunos',
+      },
+      course: 'BSCS',
+      yearAndSection: '2A',
+      username: 'Joana123',
+      password: 'Yoo01',
+      usertype: 'Student'
+    },
+    {
+      id: 4,
+      name: {
+        firstname: 'Nicole James',
+        lastname: 'Paguican',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'Jacolle21',
+      password: 'Nics123',
+      usertype: 'Student'
+    },
+    {
+      id: 5,
+      name: {
+        firstname: 'Earl Mike',
+        lastname: 'Sarabia',
+      },
+      course: 'BSIT',
+      yearAndSection: '3B',
+      username: 'EarlMike13',
+      password: 'langga08',
+      usertype: 'Teacher'
+    },
+    {
+      id: 6,
+      name: {
+        firstname: 'Marlon',
+        lastname: 'Carbonilla',
+      },
+      course: 'BSIT',
+      yearAndSection: '3B',
+      username: 'lonskie25',
+      password: 'bbygirl14',
+      usertype: 'Teacher'
+    },
+    {
+      id: 7,
+      name: {
+        firstname: 'Rodel',
+        lastname: 'Garcia',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'del06',
+      password: 'delG09',
+      usertype: 'Student'
+    }
+    
+  ]
+  
+  export default data;
